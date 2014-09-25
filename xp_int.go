0e18010966cf8e5a65f28de0107b2928b61ce2bd
Denis Zubkov <deze333@yahoo.com>
2014-09-26 08:01:07 +1000

    added int

--- xp_int.go
+++ xp_int.go
@@ -1,8 +1,27 @@
 package xparam
 
+import (
+	"fmt"
+	"strconv"
+)
+
 //------------------------------------------------------------
 // Xparam access for int
 //------------------------------------------------------------
+
+// Gets parameter as int.
+func (xp XP) As_Int(key string) (n int) {
+
+	if val, ok := xp[key]; ok && val != nil {
+		if n, ok = val.(int); ok {
+			return
+		} else {
+			str := fmt.Sprint(val)
+			n, _ = strconv.Atoi(str)
+		}
+	}
+	return
+}
 
 // Gets parameter as int array.
 func (xp XP) As_ArrayInt(key string) (data []int) {
